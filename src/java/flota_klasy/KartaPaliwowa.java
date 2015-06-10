461018e62c9243a205730775b1b930e2cda513c2
michalkrupiarz <michalkrupiarz@gmail.com>
2015-06-10 19:58:58 +0200

    adding classes

--- src/java/flota_klasy/KartaPaliwowa.java
+++ src/java/flota_klasy/KartaPaliwowa.java
@@ -18,4 +18,67 @@ public class KartaPaliwowa {
     private String pin_karty;
     private int id_importu;
     
+    public KartaPaliwowa(){
+        
+    }
+
+    public KartaPaliwowa(long id_karta_paliwowa, int id_status_paliwowa, int id_lokalizacja_paliwowa, String numer_Karty, String pin_karty, int id_importu) {
+        this.id_karta_paliwowa = id_karta_paliwowa;
+        this.id_status_paliwowa = id_status_paliwowa;
+        this.id_lokalizacja_paliwowa = id_lokalizacja_paliwowa;
+        this.numer_Karty = numer_Karty;
+        this.pin_karty = pin_karty;
+        this.id_importu = id_importu;
+    }
+
+    public long getId_karta_paliwowa() {
+        return id_karta_paliwowa;
+    }
+
+    public void setId_karta_paliwowa(long id_karta_paliwowa) {
+        this.id_karta_paliwowa = id_karta_paliwowa;
+    }
+
+    public int getId_status_paliwowa() {
+        return id_status_paliwowa;
+    }
+
+    public void setId_status_paliwowa(int id_status_paliwowa) {
+        this.id_status_paliwowa = id_status_paliwowa;
+    }
+
+    public int getId_lokalizacja_paliwowa() {
+        return id_lokalizacja_paliwowa;
+    }
+
+    public void setId_lokalizacja_paliwowa(int id_lokalizacja_paliwowa) {
+        this.id_lokalizacja_paliwowa = id_lokalizacja_paliwowa;
+    }
+
+    public String getNumer_Karty() {
+        return numer_Karty;
+    }
+
+    public void setNumer_Karty(String numer_Karty) {
+        this.numer_Karty = numer_Karty;
+    }
+
+    public String getPin_karty() {
+        return pin_karty;
+    }
+
+    public void setPin_karty(String pin_karty) {
+        this.pin_karty = pin_karty;
+    }
+
+    public int getId_importu() {
+        return id_importu;
+    }
+
+    public void setId_importu(int id_importu) {
+        this.id_importu = id_importu;
+    }
+    
+    
+    
 }
